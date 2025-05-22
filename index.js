c3fd1eb390794fd042b6063886fa18d430cd9e6a
Diya-Purkayastha <diyaphitron@gmail.com>
2025-05-22 23:52:15 +0600

    Organize routes for clarity and maintainability

--- index.js
+++ index.js
@@ -1,14 +1,12 @@
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
-const express = require('express');
-const cors = require('cors');
 require('dotenv').config();
+const express = require("express");
+const cors = require('cors');
 const app = express();
 const port = process.env.PORT || 3000;
 
 app.use(cors());
 app.use(express.json());
-
-console.log(process.env.DB_PASS);
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.9yj1zgc.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 
@@ -114,6 +112,8 @@ app.get('/', (req, res) => {
     res.send('server is working perfectly')
 })
 
-// app.listen(port, () => {
-//     console.log('server is running on ', port);
-// })
+app.listen(port, () => {
+    console.log('server is running on ', port);
+})
+
+
