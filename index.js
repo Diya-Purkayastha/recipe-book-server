5f1f56817c57dec5447ab32cb9650f44c475c627
Diya-Purkayastha <diyaphitron@gmail.com>
2025-05-23 12:19:55 +0600

    Implement PUT /recipes/:id to update a recipe

--- index.js
+++ index.js
@@ -31,40 +31,40 @@ async function run() {
         const recipeCollection = client.db('recipeDB').collection('recipe')
         const usersCollection = client.db('recipeDB').collection('users')
 
-        // 1. POST: Add new recipe
+      
     app.post('/recipe', async (req, res) => {
       const newRecipe = req.body;
       const result = await recipeCollection.insertOne(newRecipe);
       res.send(result);
     });
 
-    // 2. GET: All recipes
+   
     app.get('/recipe', async (req, res) => {
       const result = await recipeCollection.find().toArray();
       res.send(result);
     });
 
-    // 3. GET: Top 6 recipes by likes
+
     app.get('/recipe/top', async (req, res) => {
       const result = await recipeCollection.find().sort({ likeCount: -1 }).limit(6).toArray();
       res.send(result);
     });
 
-    // 4. GET: Single recipe by ID
+    
     app.get('/recipe/:id', async (req, res) => {
       const id = req.params.id;
       const result = await recipeCollection.findOne({ _id: new ObjectId(id) });
       res.send(result);
     });
 
-    // 5. GET: Recipes added by logged-in user
+  
     app.get('/myrecipe/:email', async (req, res) => {
       const email = req.params.email;
       const result = await recipeCollection.find({ userEmail: email }).toArray();
       res.send(result);
     });
 
-    // 6. PUT: Update a recipe
+   
     app.put('/recipe/:id', async (req, res) => {
       const id = req.params.id;
       const updatedData = req.body;
@@ -75,14 +75,14 @@ async function run() {
       res.send(result);
     });
 
-    // 7. DELETE: Delete a recipe
+   
     app.delete('/recipe/:id', async (req, res) => {
       const id = req.params.id;
       const result = await recipeCollection.deleteOne({ _id: new ObjectId(id) });
       res.send(result);
     });
 
-    // 8. PATCH: Increment like count
+  
     app.patch('/like/:id', async (req, res) => {
       const id = req.params.id;
       const result = await recipeCollection.updateOne(
